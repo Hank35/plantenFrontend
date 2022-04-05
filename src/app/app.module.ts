a8f54ec0bfd83d328a885424c3eb2f4ca5bb7a28
Hein Crebolder <heincrebolder@gmail.com>
2022-04-05 12:36:03 +0200

    initial push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { PlantComponent } from './plant/plant.component';
+import { ClimateComponent } from './climate/climate.component';
+import { ThreadComponent } from './thread/thread.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlantComponent,
+    ClimateComponent,
+    ThreadComponent
   ],
   imports: [
     BrowserModule,
